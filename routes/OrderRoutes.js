ddcedd09d02b805416d45ad36810ab0053c2c22a
Javier García Aguilar <javiergarciaaguilar2002@gmail.com>
2023-01-22 19:48:14 +0100

    OrderRoutes: Rutas de petición get y post de order

--- routes/OrderRoutes.js
+++ routes/OrderRoutes.js
@@ -11,7 +11,20 @@ module.exports = (options) => {
 
   // TODO: Include routes for:
   // 1. Retrieving orders from current logged-in customer
+  app.route('/orders')
+    .get(
+      middlewares.isLoggedIn,
+      middlewares.hasRole('customer'),
+      OrderController.indexCustomer
+    )
   // 2. Creating a new order (only customers can create new orders)
+  app.route('/orders')
+    .post(
+      middlewares.isLoggedIn,
+      middlewares.hasRole('customer'),
+      OrderValidation.create(),
+      OrderController.create
+    )
 
   app.route('/orders/:orderId/confirm')
     .patch(
